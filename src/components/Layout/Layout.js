e735663592dc5a2e00dfa9549067b5355bdae1d1
Claudio Paternesi <claudiopate93@gmail.com>
2020-04-05 18:46:40 +0200

    Base App Done
    Add Side Bar
    Add Nav bar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import clases from './Layou.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideDrawer</div>
-        <main className={clases.Content}>
-            {props.children}
-        </main>
+class Layout extends Component {
 
-    </Aux>
-   
-);
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+
+    render (){
+        return(
+                <Aux>
+                    <Toolbar drawerToggleClick={this.sideDrawerToggleHandler}/>
+                    <SideDrawer 
+                        open={this.state.showSideDrawer} 
+                        closed={this.sideDrawerCloseHandler}/>
+                    <main className={clases.Content}>
+                        {this.props.children}
+                    </main>
+
+                </Aux>
+        )
+    }
+};
 
 
-export default layout;
+export default Layout;
