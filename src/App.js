f1bada1cfd78666a7e3c3b52086e01c3d9cd8f10
Claudio Paternesi <claudiopate93@gmail.com>
2020-04-19 15:37:34 +0200

    Orders

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import {Route, Switch} from 'react-router-dom';
 import Checkout from './containers/Checkout/Checkout';
-
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
 
@@ -14,6 +14,7 @@ class App extends Component {
             <Layout>
               <Switch>
                 <Route path="/checkout" component={Checkout} />
+                <Route path="/orders" component={Orders} />
                 <Route path="/" exact component={BurgerBuilder} />
               </Switch>
 
