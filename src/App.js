6060d87cfe7074f6152aa3c1415ad260c2ed1d31
Claudio Paternesi <claudiopate93@gmail.com>
2020-04-19 10:24:12 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-
+import Route from 'react-router-dom';
 
 class App extends Component {
 
 
   render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
-      </div>
+        <div>  
+            <Layout>
+              <BurgerBuilder />
+            </Layout>
+        </div>
     );
   }
 }
